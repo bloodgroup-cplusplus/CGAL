3946788d50559eea07ff91d773e799a2b336bc2c
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-06-01 22:25:48 +0530

    Update three_points_and_one_segment.cpp

--- three_points_and_one_segment.cpp
+++ three_points_and_one_segment.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<CGAL/Simple_cartesian.h>
-typedef CGAL::Simple_cartesian<double> Kernal;
+typedef CGAL::Simple_cartesian<double> Kernel;
 typedef Kernel::Point_2 Point_2;
 typedef Kernel::Segment_2 Segment_2;
 
@@ -8,7 +8,7 @@ int main()
 {
 	Point_2 p(1,1), q(10,10);
 	std::cout<<"p= "<<p<<std::endl;
-	std::cout<<"q= << q.x() << "  "<<q.y()<<std::endl;
+	std::cout<<"q= "<< q.x() << "  "<<q.y()<<std::endl;
 	std::cout<<"sqdist(p,q)="<<CGAL::squared_distance(p,q)<<std::endl;
 	Segment_2 s(p,q);
 	Point_2 m(5,9);
